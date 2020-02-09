c06415207937625c819a662da65f96636371eff9
Muin Momin <momin.muin@gmail.com>
2020-02-08 18:20:51 -0800

    Removed shadow and published

--- @narative/gatsby-theme-novela/src/components/Navigation/Navigation.Footer.tsx
+++ @narative/gatsby-theme-novela/src/components/Navigation/Navigation.Footer.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import styled from "@emotion/styled";
-import { graphql, useStaticQuery } from "gatsby";
+import React from 'react';
+import styled from '@emotion/styled';
+import { graphql, useStaticQuery } from 'gatsby';
 
-import Section from "@components/Section";
-import SocialLinks from "@components/SocialLinks";
+import Section from '@components/Section';
+import SocialLinks from '@components/SocialLinks';
 
-import mediaqueries from "@styles/media";
+import mediaqueries from '@styles/media';
 
 const siteQuery = graphql`
   {
@@ -43,15 +43,15 @@ const Footer: React.FC<{}> = () => {
 
   const copyrightDate = (() => {
     const { edges } = results.allMdx;
-    const years = [0, edges.length - 1].map((edge) =>
-      new Date(edges[edge].node.frontmatter.date).getFullYear()
+    const years = [0, edges.length - 1].map(edge =>
+      new Date(edges[edge].node.frontmatter.date).getFullYear(),
     );
     return years[0] === years[1] ? `${years[0]}` : `${years[0]}–${years[1]}`;
   })();
 
   return (
     <>
-      <FooterGradient />
+      {/* <FooterGradient /> */}
       <Section narrow>
         <HoritzontalRule />
         <FooterContainer>
@@ -111,14 +111,14 @@ const FooterText = styled.div`
   `}
 `;
 
-const FooterGradient = styled.div`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 590px;
-  z-index: 0;
-  pointer-events: none;
-  background: ${p => p.theme.colors.gradient};
-  transition: ${p => p.theme.colorModeTransition};
-`;
+// const FooterGradient = styled.div`
+//   position: absolute;
+//   bottom: 0;
+//   left: 0;
+//   width: 100%;
+//   height: 590px;
+//   z-index: 0;
+//   pointer-events: none;
+//   background: ${p => p.theme.colors.gradient};
+//   transition: ${p => p.theme.colorModeTransition};
+// `;
--- @narative/gatsby-theme-novela/src/templates/articles.template.tsx
+++ @narative/gatsby-theme-novela/src/templates/articles.template.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import styled from "@emotion/styled";
+import React from 'react';
+import styled from '@emotion/styled';
 
-import Section from "@components/Section";
-import SEO from "@components/SEO";
-import Layout from "@components/Layout";
-import Paginator from "@components/Navigation/Navigation.Paginator";
+import Section from '@components/Section';
+import SEO from '@components/SEO';
+import Layout from '@components/Layout';
+import Paginator from '@components/Navigation/Navigation.Paginator';
 
-import ArticlesHero from "../sections/articles/Articles.Hero";
-import ArticlesList from "../sections/articles/Articles.List";
+import ArticlesHero from '../sections/articles/Articles.Hero';
+import ArticlesList from '../sections/articles/Articles.List';
 
-import { Template } from "@types";
+import { Template } from '@types';
 
 const ArticlesPage: Template = ({ location, pageContext }) => {
   const articles = pageContext.group;
@@ -25,24 +25,24 @@ const ArticlesPage: Template = ({ location, pageContext }) => {
           <Paginator {...pageContext} />
         </ArticlesPaginator>
       </Section>
-      <ArticlesGradient />
+      {/* <ArticlesGradient /> */}
     </Layout>
   );
 };
 
 export default ArticlesPage;
 
-const ArticlesGradient = styled.div`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 590px;
-  z-index: 0;
-  pointer-events: none;
-  background: ${p => p.theme.colors.gradient};
-  transition: ${p => p.theme.colorModeTransition};
-`;
+// const ArticlesGradient = styled.div`
+//   position: absolute;
+//   bottom: 0;
+//   left: 0;
+//   width: 100%;
+//   height: 590px;
+//   z-index: 0;
+//   pointer-events: none;
+//   background: ${p => p.theme.colors.gradient};
+//   transition: ${p => p.theme.colorModeTransition};
+// `;
 
 const ArticlesPaginator = styled.div<{ show: boolean }>`
   ${p => p.show && `margin-top: 95px;`}
